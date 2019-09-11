3a3c777219e18eab7ed6d7d7f0b102102b5fa665
Edgardo <emoron@gmail.com>
2019-09-11 12:33:02 -0500

    Update color-grayscale.py

--- color-grayscale.py
+++ color-grayscale.py
@@ -6,6 +6,9 @@ image = cv2.imread('C:/Users/N/Desktop/Test.jpg')
 
 # Convert Color to GrayScale
 grayScale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# Convert to Black and White
+
+(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
 
 cv.imshow('Original Image',image)
 cv.imshow('Gray Scale',grayScale)
